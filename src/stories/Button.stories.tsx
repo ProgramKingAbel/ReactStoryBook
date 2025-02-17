7fb09c1ca8617c3ce1d347b2c3477b5f516cbafe
Abel Morara <abelmorara254@gmail.com>
2025-02-17 16:25:19 +0300

    chore: renaming stories

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -9,3 +9,9 @@ export default {
 export const Primary = () => <Button variant='primary'>Primary</Button>;
 export const Secondary = () => <Button variant='secondary' >Secondary</Button>
 export const Danger = () => <Button variant='danger'>Danger</Button>
+
+//Renaming Stories
+
+Primary.storyName = "Blue Button"
+Secondary.storyName = "Green Button"
+Danger.storyName = "Red Button"
