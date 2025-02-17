a63047284acc02cdc44079f49c1d6cda7484c027
Abel Morara <abelmorara254@gmail.com>
2025-02-17 16:20:43 +0300

    feat: implement variations of a component

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -6,4 +6,6 @@ export default {
     component: Button
 }
 
-export const Primary = () => <Button />;
+export const Primary = () => <Button variant='primary'>Primary</Button>;
+export const Secondary = () => <Button variant='secondary' >Secondary</Button>
+export const Danger = () => <Button variant='danger'>Danger</Button>
